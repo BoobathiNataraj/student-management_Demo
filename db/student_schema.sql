62e55e374553efdb6d8066ab2f2e27df388ea6ce
BoobathiKumar <boobathikumarn1106@gmail.com>
2026-02-02 15:00:36 +0530

    Create student_schema.sql

---
+++ db/student_schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `student` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `age` int NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `marks` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
